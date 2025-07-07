13776538822687fa1737bc43a4358cbbf81044c4
legale <32621719+legale@users.noreply.github.com>
2025-07-07 21:44:42 +0300

    netlink_arp_cache: add DI init

--- netlink_arp_cache/test.c
+++ netlink_arp_cache/test.c
@@ -1,11 +1,48 @@
 #include "libnlarpcache.h"
+#include "../syslog2/syslog2.h"
 
 #include <assert.h>
 #include <linux/rtnetlink.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <time.h>
+
+static int log_called = 0;
+static int time_called = 0;
+
+static void mock_log(int pri, const char *fmt, ...) {
+  (void)pri;
+  (void)fmt;
+  log_called++;
+}
+
+static int mock_time(struct timespec *ts) {
+  if (ts) {
+    ts->tv_sec = 0;
+    ts->tv_nsec = 0;
+  }
+  time_called++;
+  return 0;
+}
+
+#ifdef TESTRUN
+int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
+           struct timeval *timeout) {
+  (void)nfds;
+  (void)readfds;
+  (void)writefds;
+  (void)exceptfds;
+  (void)timeout;
+  errno = EINTR;
+  return -1;
+}
+#endif
 
 int main(void) {
+  nlarpcache_mod_init(&(netlink_arp_cache_mod_init_args_t){
+      .log = mock_log, .get_time = mock_time});
+
   unsigned char buf[256];
 
   struct rtattr *rta1 = (struct rtattr *)buf;
@@ -21,9 +58,26 @@ int main(void) {
   unsigned len = RTA_SPACE(4) + RTA_SPACE(6);
   struct rtattr *tb[NDA_MAX + 1] = {0};
   parse_rtattr(tb, NDA_MAX, rta1, len);
+  assert(time_called == 1);
 
   assert(tb[NDA_DST] == rta1);
   assert(tb[NDA_LLADDR] == rta2);
+
+  void *rb = NULL;
+  struct {
+    struct nlmsghdr n;
+    struct ndmsg ndm;
+    char buf[0];
+  } req = {
+      .n.nlmsg_len = NLMSG_LENGTH(sizeof(struct ndmsg)),
+      .n.nlmsg_type = RTM_GETNEIGH,
+      .n.nlmsg_flags = NLM_F_REQUEST | NLM_F_ROOT,
+  };
+
+  send_recv(&req, req.n.nlmsg_len, &rb);
+  free(rb);
+
+  assert(log_called == 1);
 
   printf("All tests passed\n");
   return 0;
